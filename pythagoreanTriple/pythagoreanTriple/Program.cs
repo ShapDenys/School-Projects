c488a4b36a03f02ab2fcd4045f77ac78a67e6031
ShapDenys <denys.shapovalov@outlook.com>
2025-03-12 12:51:50 +0100

    Add files via upload
    pythagoreanTriple

---
+++ pythagoreanTriple/pythagoreanTriple/Program.cs
@@ -0,0 +1,25 @@
+﻿try
+{
+    Console.WriteLine("Wprowadź pierwszą liczbę: ");
+    int num1 = int.Parse(Console.ReadLine());
+    Console.WriteLine("Wprowadź drugą liczbę: ");
+    int num2 = int.Parse(Console.ReadLine());
+    Console.WriteLine("Wprowadź trzecią liczbę: ");
+    int num3 = int.Parse(Console.ReadLine());
+
+    int[] arr = { num1, num2, num3 };
+    Array.Sort(arr);
+
+    if (Math.Pow(arr[0], 2) + Math.Pow(arr[1], 2) == Math.Pow(arr[2], 2))
+    {
+        Console.WriteLine("Z tego wyjdzie trójkąt prostokątny");
+    }
+    else
+    {
+        Console.WriteLine("Z tego nie wyjdzie trójkąt");
+    }
+}
+catch (Exception e)
+{
+    Console.WriteLine(e.Message);
+}

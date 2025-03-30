a91ab23967fad09e63cdc1ac4fbb06c0915278dc
ShapDenys <denys.shapovalov@outlook.com>
2025-03-30 21:05:18 +0200

    Update Program.cs
    Changed from taking and parsing each input, to make the same, but with using for loop

--- pythagoreanTriple/pythagoreanTriple/Program.cs
+++ pythagoreanTriple/pythagoreanTriple/Program.cs
@@ -1,22 +1,21 @@
-﻿try
+try
 {
-    Console.WriteLine("Wprowadź pierwszą liczbę: ");
-    int num1 = int.Parse(Console.ReadLine());
-    Console.WriteLine("Wprowadź drugą liczbę: ");
-    int num2 = int.Parse(Console.ReadLine());
-    Console.WriteLine("Wprowadź trzecią liczbę: ");
-    int num3 = int.Parse(Console.ReadLine());
+    int[] arr = new int[3];
+    for (int i = 0; i < 3; i++)
+    {
+        Console.WriteLine($"Podaj {i + 1} liczbę: ");
+        arr[i] = int.Parse(Console.ReadLine());
+    }
 
-    int[] arr = { num1, num2, num3 };
     Array.Sort(arr);
 
     if (Math.Pow(arr[0], 2) + Math.Pow(arr[1], 2) == Math.Pow(arr[2], 2))
     {
-        Console.WriteLine("Z tego wyjdzie trójkąt prostokątny");
+        Console.WriteLine("Wyjdzie trójkąt prostokątny");
     }
     else
     {
-        Console.WriteLine("Z tego nie wyjdzie trójkąt prostokątny");
+        Console.WriteLine("Nie wyjdzie trójkąt prostokątny");
     }
 }
 catch (Exception e)
